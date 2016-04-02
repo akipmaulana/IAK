b5b71e5c8fe301abbac9d1828bfb093f810cd100
Akip Maulana <akip.maulana@gmail.com>
2016-04-02 11:17:57 +0700

    Initial Sunshine

---
+++ Sunshine/app/src/main/java/iak/development/akirah/sunshine/HomeActivity.java
@@ -0,0 +1,23 @@
+package iak.development.akirah.sunshine;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ListView;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+//        ListView listView = (ListView) findViewById(R.id.)
+    }
+
+}
